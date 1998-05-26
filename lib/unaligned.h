b1487ee909ebd4cfc59f30d3678cb6667d4a72c8
Martin Mares <mj@ucw.cz>
1998-05-26 21:38:06 +0000

    Added generic functions for unaligned data access.

--- lib/unaligned.h
+++ lib/unaligned.h
@@ -1,5 +1,5 @@
 /*
- *	Unaligned Data Accesses
+ *	Unaligned Data Accesses -- Generic Version
  *
  *	(c) 1998 Martin Mares <mj@ucw.cz>
  *
@@ -8,5 +8,35 @@
 
 #ifndef _BIRD_UNALIGNED_H_
 #define _BIRD_UNALIGNED_H_
+
+#if CPU_NEEDS_ALIGN_WORD != 1 || CPU_NEEDS_ALIGN_LONG != 1
+#include <string.h>
+#endif
+
+#if CPU_NEEDS_ALIGN_WORD == 1
+#define unaligned_u16(p) (*((u16 *)(p)))
+#else
+static inline u16
+unaligned_u16(void *p)
+{
+  u16 x;
+
+  memcpy(&x, p, sizeof(x));
+  return x;
+}
+#endif
+
+#if CPU_NEEDS_ALIGN_LONG == 1
+#define unaligned_u32(p) (*((u32 *)(p)))
+#else
+static inline u32
+unaligned_u32(void *p)
+{
+  u32 x;
+
+  memcpy(&x, p, sizeof(x));
+  return x;
+}
+#endif
 
 #endif
