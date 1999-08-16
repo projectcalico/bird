b332fcdfc8f0b1ac7111a63c55f72ba4b00b8035
Ondrej Filip <feela@network.cz>
1999-08-16 10:32:05 +0000

    Better dumping.

--- proto/ospf/ospf.c
+++ proto/ospf/ospf.c
@@ -825,11 +825,23 @@ static void
 ospf_dump(struct proto *p)
 {
   char areastr[20];
+  struct ospf_iface *ifa;
+  struct ospf_neighbor *n;
   struct ospf_config *c = (void *) p->cf;
 
-  DBG(p->name);
-  DBG(": Dump.\n");
-  debug(" -AreaID: %u\n", c->area );
+  debug("%s: AreaID: %u\n", p->name, c->area );
+
+  WALK_LIST(ifa, ((struct proto_ospf *)p)->iface_list)
+  {
+    debug("%s: Interface: %s\n", p->name, ifa->iface->name);
+    debug("%s:  state: %u\n", p->name, ifa->state);
+    debug("%s:  DR:  %u\n", p->name, ifa->drid);
+    debug("%s:  BDR: %u\n", p->name, ifa->bdrid);
+    WALK_LIST(n, ifa->neigh_list)
+    {
+      debug("%s:   neighbor %u in state %u\n", p->name, n->rid, n->state);
+    }
+  }
 }
 
 static struct proto *
