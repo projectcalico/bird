f611f0ee824a6b363dc675e8f6ac963f20e7d007
Martin Mares <mj@ucw.cz>
1999-12-01 15:07:56 +0000

    Reset temporary parser data before parsing, not afterwards. This enables deferred CLI command handlers to store their temporary data in the CLI parsing pool.

--- nest/cli.c
+++ nest/cli.c
@@ -128,8 +128,8 @@ cli_command(struct cli *c)
   cli_rh_len = strlen(c->rx_buf);
   cli_rh_trick_flag = 0;
   this_cli = c;
-  res = cli_parse(&f);
   lp_flush(c->parser_pool);
+  res = cli_parse(&f);
   if (!res)
     cli_printf(c, 9001, f.err_msg);
 }
