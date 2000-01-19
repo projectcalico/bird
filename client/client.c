f50b9e48b93e3f69423a0e6e5fef273ba2022958
Martin Mares <mj@ucw.cz>
2000-01-19 14:37:56 +0000

    Generate a list of all commands and their help texts for the client to use.

--- client/client.c
+++ client/client.c
@@ -1,13 +1,23 @@
 /*
  *	BIRD Client
  *
- *	(c) 1999 Martin Mares <mj@ucw.cz>
+ *	(c) 1999--2000 Martin Mares <mj@ucw.cz>
  *
  *	Can be freely distributed and used under the terms of the GNU GPL.
  */
 
 #include "nest/bird.h"
 #include "client/client.h"
+
+struct cmd_info {
+  char *command;
+  char *args;
+  char *help;
+};
+
+static struct cmd_info command_table[] = {
+#include "conf/commands.h"
+};
 
 int
 main(int argc, char **argv)
