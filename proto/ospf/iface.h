6fa948d6c5abd0620836f66c654354ce38936f3a
Ondrej Filip <feela@network.cz>
2000-02-23 23:13:27 +0000

    Better debugging.

--- proto/ospf/iface.h
+++ proto/ospf/iface.h
@@ -16,7 +16,7 @@ void ospf_int_sm(struct ospf_iface *ifa, int event);
 sock *ospf_open_mc_socket(struct ospf_iface *ifa);
 sock *ospf_open_ip_socket(struct ospf_iface *ifa);
 u8 is_good_iface(struct proto *p, struct iface *iface);
-u8 ospf_iface_clasify(struct iface *ifa);
+u8 ospf_iface_clasify(struct iface *ifa, struct proto *p);
 void ospf_add_timers(struct ospf_iface *ifa, pool *pool);
 void ospf_iface_default(struct ospf_iface *ifa);
 struct ospf_iface *find_iface(struct proto_ospf *p, struct iface *what);
