d27d0efe17a197ae41e25bc56526d5fc4ac4a93e
Ondrej Filip <feela@network.cz>
2000-06-01 00:22:48 +0000

    Very stuping bug. (I took me 4 hours to find it!) I just wrote "=!" instead of "!=". :-(((((((

--- proto/ospf/lsalib.c
+++ proto/ospf/lsalib.c
@@ -357,7 +357,7 @@ lsa_comp(struct ospf_lsa_header *l1, struct ospf_lsa_header *l2)
   if(sn1>sn2) return CMP_NEWER;
   if(sn1<sn2) return CMP_OLDER;
 
-  if(l1->checksum=!l2->checksum)
+  if(l1->checksum!=l2->checksum)
     return l1->checksum<l2->checksum ? CMP_OLDER : CMP_NEWER;
 
   if((l1->age==LSA_MAXAGE)&&(l2->age!=LSA_MAXAGE)) return CMP_NEWER;
