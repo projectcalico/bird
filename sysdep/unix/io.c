10304bed435034cf8432b1c6262f7e7f0d84d49c
Martin Mares <mj@ucw.cz>
2000-06-05 11:46:40 +0000

    Split random number functions off io.c, so that they can be documented separately.

--- sysdep/unix/io.c
+++ sysdep/unix/io.c
@@ -29,20 +29,6 @@
 #include "lib/sysio.h"
 
 /*
- *	Random Numbers
- */
-
-u32
-random_u32(void)
-{
-  long int rand_low, rand_high;
-
-  rand_low = random();
-  rand_high = random();
-  return (rand_low & 0xffff) | ((rand_high & 0xffff) << 16);
-}
-
-/*
  *	Tracked Files
  */
 
