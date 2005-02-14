75c1c585197f2b4a1b0295d36fe16a4869c21914
Ondrej Filip <feela@network.cz>
2005-02-14 11:37:40 +0000

    Yes, I'd like to go to 1.0.10

--- sysdep/config.h
+++ sysdep/config.h
@@ -7,7 +7,7 @@
 #define _BIRD_CONFIG_H_
 
 /* BIRD version */
-#define BIRD_VERSION "1.0.9"
+#define BIRD_VERSION "1.0.10"
 
 /* Include parameters determined by configure script */
 #include "sysdep/autoconf.h"
