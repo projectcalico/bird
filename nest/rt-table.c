4ca93a50675489a6a5ef9369c24806cc3cbff45e
Ondrej Zajicek <santiago@crfreenet.org>
2010-09-08 12:08:38 +0200

    Fixes a one byte buffer overflow.

--- nest/rt-table.c
+++ nest/rt-table.c
@@ -1637,7 +1637,7 @@ rt_format_via(rte *e, byte *via)
 static void
 rt_show_rte(struct cli *c, byte *ia, rte *e, struct rt_show_data *d, ea_list *tmpa)
 {
-  byte via[STD_ADDRESS_P_LENGTH+32], from[STD_ADDRESS_P_LENGTH+6];
+  byte via[STD_ADDRESS_P_LENGTH+32], from[STD_ADDRESS_P_LENGTH+8];
   byte tm[TM_DATETIME_BUFFER_SIZE], info[256];
   rta *a = e->attrs;
   int primary = (e->net->routes == e);
