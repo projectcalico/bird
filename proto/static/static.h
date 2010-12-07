9852f81064a38d35ff1bd5cc9fab7fc33926c83c
Ondrej Zajicek <santiago@crfreenet.org>
2010-12-07 23:34:36 +0100

    Multipath support for static protocol.

--- proto/static/static.h
+++ proto/static/static.h
@@ -28,8 +28,12 @@ struct static_route {
   ip_addr via;				/* Destination router */
   struct neighbor *neigh;
   byte *if_name;			/* Name for RTD_DEVICE routes */
+  struct static_route *mp_next;		/* Nexthops for RTD_MULTIPATH routes */
   int installed;			/* Installed in master table */
 };
+
+/* Dummy nodes (parts of multipath route) abuses masklen field for weight
+   and if_name field for a ptr to the master (RTD_MULTIPATH) node. */
 
 void static_show(struct proto *);
 
