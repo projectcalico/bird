b7c48981069f25c01c552519e10aec4ebab1f031
Ondrej Filip <feela@network.cz>
2011-05-05 14:14:20 +0200

    Compilation was failing without OSPF or RIP protocol - FIXED.

--- nest/rt-table.c
+++ nest/rt-table.c
@@ -1486,13 +1486,18 @@ rt_get_igp_metric(rte *rt)
     return ea->u.data;
 
   rta *a = rt->attrs;
+
+#ifdef CONFIG_OSPF
   if ((a->source == RTS_OSPF) ||
       (a->source == RTS_OSPF_IA) ||
       (a->source == RTS_OSPF_EXT1))
     return rt->u.ospf.metric1;
+#endif
 
+#ifdef CONFIG_RIP
   if (a->source == RTS_RIP)
     return rt->u.rip.metric;
+#endif
 
   /* Device routes */
   if ((a->dest != RTD_ROUTER) && (a->dest != RTD_MULTIPATH))
