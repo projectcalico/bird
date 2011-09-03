6c4df70373fa640a7f068d4e1b563621b67c0d2b
Ondrej Zajicek <santiago@crfreenet.org>
2011-09-03 21:59:40 +0200

    Fixes possible buffer overflow when printing BGP attributes.
    Thanks to Alexander V. Chernikov for the patch.

--- proto/bgp/attrs.c
+++ proto/bgp/attrs.c
@@ -1576,16 +1576,18 @@ bgp_get_attr(eattr *a, byte *buf, int buflen)
 {
   unsigned int i = EA_ID(a->id);
   struct attr_desc *d;
+  int len;
 
   if (ATTR_KNOWN(i))
     {
       d = &bgp_attr_table[i];
-      buf += bsprintf(buf, "%s", d->name);
+      len = bsprintf(buf, "%s", d->name);
+      buf += len;
       if (d->format)
 	{
 	  *buf++ = ':';
 	  *buf++ = ' ';
-	  d->format(a, buf, buflen);
+	  d->format(a, buf, buflen - len - 2);
 	  return GA_FULL;
 	}
       return GA_NAME;
