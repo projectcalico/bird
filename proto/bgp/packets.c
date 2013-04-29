572c6440432e3138ea622cfb5a4ef7580d77ef4a
Ondrej Zajicek <santiago@crfreenet.org>
2013-04-29 22:08:05 +0200

    Fixes a crash when mrtdump is enabled and interface goes away.
    Thanks to Peter Christensen for the bugfix.

--- proto/bgp/packets.c
+++ proto/bgp/packets.c
@@ -58,7 +58,7 @@ mrt_put_bgp4_hdr(byte *buf, struct bgp_conn *conn, int as4)
       buf+=4;
     }
 
-  put_u16(buf+0, p->neigh ? p->neigh->iface->index : 0);
+  put_u16(buf+0, (p->neigh && p->neigh->iface) ? p->neigh->iface->index : 0);
   put_u16(buf+2, BGP_AF);
   buf+=4;
   buf = ipa_put_addr(buf, conn->sk ? conn->sk->daddr : IPA_NONE);
