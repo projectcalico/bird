9810d055628877232f811d684567e203381e10dc
Ondrej Zajicek <santiago@crfreenet.org>
2013-05-28 10:44:44 +0200

    Fixes problems with routing table scans on some platforms.
    Negative bit shifts are definitely undefined oprations.

--- sysdep/linux/netlink.c
+++ sysdep/linux/netlink.c
@@ -843,9 +843,11 @@ nl_parse_route(struct nlmsghdr *h, int scan)
 	  memcpy(&ra.gw, RTA_DATA(a[RTA_GATEWAY]), sizeof(ra.gw));
 	  ipa_ntoh(ra.gw);
 
+#ifdef IPV6
 	  /* Silently skip strange 6to4 routes */
 	  if (ipa_in_net(ra.gw, IPA_NONE, 96))
 	    return;
+#endif
 
 	  ng = neigh_find2(&p->p, &ra.gw, ra.iface,
 			   (i->rtm_flags & RTNH_F_ONLINK) ? NEF_ONLINK : 0);
