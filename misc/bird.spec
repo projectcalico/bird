e75520c455a1a3fd026a9633c3ad26d865165fee
Ondrej Zajicek <santiago@crfreenet.org>
2013-11-25 13:46:29 +0100

    NEWS and version update.

--- misc/bird.spec
+++ misc/bird.spec
@@ -1,6 +1,6 @@
 Summary: BIRD Internet Routing Daemon
 Name: bird
-Version: 1.3.12
+Version: 1.4.0
 Release: 1
 Copyright: GPL
 Group: Networking/Daemons
--- sysdep/config.h
+++ sysdep/config.h
@@ -7,7 +7,7 @@
 #define _BIRD_CONFIG_H_
 
 /* BIRD version */
-#define BIRD_VERSION "1.3.12"
+#define BIRD_VERSION "1.4.0"
 
 /* Include parameters determined by configure script */
 #include "sysdep/autoconf.h"
