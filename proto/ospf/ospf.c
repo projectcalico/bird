10c2e1e0125dfa600b1c2b6553883cccf2d025dd
Ondrej Zajicek <santiago@crfreenet.org>
2014-02-06 17:54:25 +0100

    Fixes crash when state of disabled OSPF protocol is queried.
    Thanks to Ondrej Caletka for the bugreport.

--- proto/ospf/ospf.c
+++ proto/ospf/ospf.c
@@ -1276,7 +1276,6 @@ ospf_sh_state(struct proto *p, int verbose, int reachable)
 {
   struct proto_ospf *po = (struct proto_ospf *) p;
   struct ospf_lsa_header *cnode = NULL;
-  int num = po->gr->hash_entries;
   unsigned int i, ix, j1, j2, jx;
   u32 last_area = 0xFFFFFFFF;
 
@@ -1290,6 +1289,7 @@ ospf_sh_state(struct proto *p, int verbose, int reachable)
   /* We store interesting area-scoped LSAs in array hea and 
      global-scoped (LSA_T_EXT) LSAs in array hex */
 
+  int num = po->gr->hash_entries;
   struct top_hash_entry *hea[num];
   struct top_hash_entry *hex[verbose ? num : 0];
   struct top_hash_entry *he;
