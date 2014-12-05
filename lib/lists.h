85a3639d99a39a79b65c2dbc2a136baee6ba2a2b
Pavel Tvrdik <pawel.tvrdik@gmail.cz>
2014-12-05 09:25:38 +0100

    Better Clang compatibility

--- lib/lists.h
+++ lib/lists.h
@@ -50,6 +50,14 @@ typedef struct list {			/* In fact two overlayed nodes */
 
 #define EMPTY_LIST(list) (!(list).head->next)
 
+
+#ifndef _BIRD_LISTS_C_
+#define LIST_INLINE static inline
+#include "lib/lists.c"
+#undef LIST_INLINE
+
+#else /* _BIRD_LISTS_C_ */
+#define LIST_INLINE
 void add_tail(list *, node *);
 void add_head(list *, node *);
 void rem_node(node *);
@@ -57,13 +65,6 @@ void rem2_node(node *);
 void add_tail_list(list *, list *);
 void init_list(list *);
 void insert_node(node *, node *);
-
-#ifndef _BIRD_LISTS_C_
-#define LIST_INLINE extern inline
-#include "lib/lists.c"
-#undef LIST_INLINE
-#else
-#define LIST_INLINE
 #endif
 
 #endif
