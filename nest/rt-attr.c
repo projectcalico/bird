12640c149976e1eca54d9c22c593d07a27c49d42
Ondrej Zajicek (work) <santiago@crfreenet.org>
2016-07-19 12:16:51 +0200

    Babel: Documentation updates
    This updates the documentation to correctly mention Babel when protocols
    are listed, and adds examples and route attribute documentation to the
    Babel section of the docs.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@toke.dk>

--- nest/rt-attr.c
+++ nest/rt-attr.c
@@ -1160,7 +1160,7 @@ rta_dump(rta *a)
   static char *rts[] = { "RTS_DUMMY", "RTS_STATIC", "RTS_INHERIT", "RTS_DEVICE",
 			 "RTS_STAT_DEV", "RTS_REDIR", "RTS_RIP",
 			 "RTS_OSPF", "RTS_OSPF_IA", "RTS_OSPF_EXT1",
-                         "RTS_OSPF_EXT2", "RTS_BGP" };
+                         "RTS_OSPF_EXT2", "RTS_BGP", "RTS_PIPE", "RTS_BABEL" };
   static char *rtc[] = { "", " BC", " MC", " AC" };
   static char *rtd[] = { "", " DEV", " HOLE", " UNREACH", " PROHIBIT" };
 
