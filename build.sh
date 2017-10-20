0f1b8f49d85dbd57b7b0637c64b86d82f23d6cbb
David J. Wilder <wilder@us.ibm.com>
2017-10-20 11:58:44 -0700

    Adding support for ppc64le and multi-arch builds.
    The build architecture is select by setting the ARCH environment
    variable. For example: When building on ppc64le you could use:
    ARCH=ppc64le ./build.sh.
    
    When ARCH is undefined builds defaults to amd64.
    
    Signed-off-by: David Wilder <wilder@us.ibm.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,22 @@
-docker build -t birdbuild .
-mkdir -p dist
-docker run --name bird-build -v `pwd`:/code birdbuild ./create_binaries.sh
+#!/bin/bash
+###############################################################################
+# The build architecture is select by setting the ARCH variable.
+# For example: When building on ppc64le you could use ARCH=ppc64le ./build.sh.
+# When ARCH is undefined it defaults to amd64.
+###############################################################################
+[ ! $ARCH ] &&  ARCH=amd64
+
+if [ $ARCH = amd64 ]; then
+	unset ARCHTAG
+fi
+
+if [ $ARCH = ppc64le ]; then
+	ARCHTAG=-ppc64le
+fi
+
+DIST=dist/$ARCH
+
+docker build -t birdbuild$ARCHTAG -f Dockerfile$ARCHTAG . 
+mkdir -p $DIST
+docker run --name bird-build -e ARCH=$ARCH -e DIST=$DIST -v `pwd`:/code birdbuild$ARCHTAG ./create_binaries.sh
 docker rm -f bird-build || true
