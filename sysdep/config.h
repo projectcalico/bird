ef8974b7ca7595bc2685b222aa4822c13349a2e1
Ondrej Zajicek (work) <santiago@crfreenet.org>
2019-01-05 00:37:31 +0100

    NEWS and version update

--- sysdep/config.h
+++ sysdep/config.h
@@ -7,7 +7,7 @@
 #define _BIRD_CONFIG_H_
 
 /* BIRD version */
-#define BIRD_VERSION "1.6.4"
+#define BIRD_VERSION "1.6.5"
 
 /* Include parameters determined by configure script */
 #include "sysdep/autoconf.h"
